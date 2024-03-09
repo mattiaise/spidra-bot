94ec6d3351638b5658bc8964c63cb0e075c47f9a
Andrea0304 <andreapanetta03@gmail.com>
2024-03-09 18:36:17 +0100

    modified website esp

--- src/script.js
+++ src/script.js
@@ -2,7 +2,6 @@ const fudgeFactor = 2;  // because of bug in Chrome related to svg text alignmen
 const runningElem = document.querySelector('#running');
 const gamepadsElem = document.querySelector('#gamepads');
 const gamepadsByIndex = {};
-
 const controllerTemplate = `
     <div>
       <div class="head"><div class="index"></div><div class="id"></div></div>
@@ -22,7 +21,6 @@ const axisTemplate = `
         <text text-anchor="middle" fill="#CCC" x="0" y="2">0</text>
     </svg>
     `
-
 const buttonTemplate = `
     <svg viewBox="-2.2 -2.2 4.4 4.4" width="64" height="64">
       <circle cx="0" cy="0" r="2" fill="none" stroke="#888" stroke-width="0.1" />
@@ -31,6 +29,24 @@ const buttonTemplate = `
       <text class="index" alignment-baseline="hanging" dominant-baseline="hanging" text-anchor="start" fill="#CCC" x="-2" y="-2">0</text>
     </svg>
     `;
+
+function handleConnect(e) {
+    console.log('connect');
+    addGamepadIfNew(e.gamepad);
+}
+
+function addGamepadIfNew(gamepad) {
+    const info = gamepadsByIndex[gamepad.index];
+    if (!info) {
+        addGamepad(gamepad);
+    } else {
+        // This broke sometime in the past. It used to be
+        // the same gamepad object was returned forever.
+        // Then Chrome only changed to a new gamepad object
+        // is returned every frame.
+        info.gamepad = gamepad;
+    }
+}
 
 function addGamepad(gamepad) {
     console.log('add:', gamepad.index);
@@ -84,39 +100,24 @@ function removeGamepad(gamepad) {
     }
 }
 
-function addGamepadIfNew(gamepad) {
-    const info = gamepadsByIndex[gamepad.index];
-    if (!info) {
-        addGamepad(gamepad);
-    } else {
-        // This broke sometime in the past. It used to be
-        // the same gamepad object was returned forever.
-        // Then Chrome only changed to a new gamepad object
-        // is returned every frame.
-        info.gamepad = gamepad;
-    }
-}
-
-function handleConnect(e) {
-    console.log('connect');
-    addGamepadIfNew(e.gamepad);
-}
-
 function handleDisconnect(e) {
     console.log('disconnect');
     removeGamepad(e.gamepad);
 }
 
+/*
 const t = String.fromCharCode(0x26AA);
 const f = String.fromCharCode(0x26AB);
 function onOff(v) {
     return v ? t : f;
 }
+*/
 
 const keys = ['index', 'id', 'connected', 'mapping', /*'timestamp'*/];
 function processController(info) {
     const { elem, gamepad, axes, buttons } = info;
     const lines = [`gamepad  : ${gamepad.index}`];
+    console.log(axes);
     for (const key of keys) {
         info[key].textContent = gamepad[key];
     }
@@ -133,8 +134,8 @@ function processController(info) {
         value.textContent = `${button.value.toFixed(2)}`;
     });
 
-    //  lines.push(`axes     : [${gamepad.axes.map((v, ndx) => `${ndx}: ${v.toFixed(2).padStart(5)}`).join(', ')} ]`);
-    //  lines.push(`buttons  : [${gamepad.buttons.map((v, ndx) => `${ndx}: ${onOff(v.pressed)} ${v.value.toFixed(2)}`).join(', ')} ]`);
+    // lines.push(`axes     : [${gamepad.axes.map((v, ndx) => `${ndx}: ${v.toFixed(2).padStart(5)}`).join(', ')} ]`);
+    // lines.push(`buttons  : [${gamepad.buttons.map((v, ndx) => `${ndx}: ${onOff(v.pressed)} ${v.value.toFixed(2)}`).join(', ')} ]`);
     // elem.textContent = lines.join('\n');
 }
 
@@ -148,9 +149,6 @@ function addNewPads() {
     }
 }
 
-window.addEventListener("gamepadconnected", handleConnect);
-window.addEventListener("gamepaddisconnected", handleDisconnect);
-
 function process() {
     runningElem.textContent = ((performance.now() * 0.001 * 60 | 0) % 100).toString().padStart(2, '0');
     addNewPads();  // some browsers add by polling, others by event
@@ -158,4 +156,7 @@ function process() {
     Object.values(gamepadsByIndex).forEach(processController);
     requestAnimationFrame(process);
 }
+
+window.addEventListener("gamepadconnected", handleConnect);
+window.addEventListener("gamepaddisconnected", handleDisconnect);
 requestAnimationFrame(process);
